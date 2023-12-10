72c113037cf50c39dffdb6a2a28856e4088b5e63
Deathdrummer <deathdrumer@yandex.ru>
2023-12-10 08:06:49 +0300

    вернул section_contacts.tpl

--- repository/js/plugins/ddrImgLoaded.js
+++
@@ -1,8 +0,0 @@
-;$.fn.imgLoaded = function(func) {
-	var i = new Image(),
-		imageSrc = $(this).attr('src');
-	i.onload = function() {
-		if (typeof func == 'function') func();
-	}
-	i.src = imageSrc;
-};
